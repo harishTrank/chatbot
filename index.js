1a4c3aabb55f45a311fd46b8e1ab6288b1b94baa
harish1907 <nehasharma@tranktechies.com>
2024-04-30 11:19:21 +0530

    update

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected!"))
   .then(() => {
-    const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 7000;
     http.listen(PORT, console.log("Server Started on port"));
     socketMessaging(io);
   })
