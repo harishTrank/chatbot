8cdce0842bae7102aef4eece63fb5ddc8ec44f60
harish1907 <nehasharma@tranktechies.com>
2024-04-30 14:28:28 +0530

    update

--- routes/api/Auth.route.js
+++ routes/api/Auth.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const registerUser = require("../../controller/auth/registerUser");
 const loginUser = require("../../controller/auth/loginUser");
 const byPassLoginUser = require("../../controller/auth/byPassLoginUser");
+const sessionStorage = require("../../controller/auth/sessionStorage");
 
 router.post("/register", registerUser);
 router.post("/login/user", loginUser);
